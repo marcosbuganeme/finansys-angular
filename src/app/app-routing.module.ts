8541080b601e9a5920017c600509842ccf0a6039
Marcos Buganeme <marcos.after@gmail.com>
2019-04-11 16:47:15 -0300

    route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
+    { path: '', redirectTo: '/reports', pathMatch: 'full' },
     { path: 'reports', loadChildren: './pages/reports/reports.module#ReportsModule' },
     { path: 'entries', loadChildren: './pages/entries/entries.module#EntriesModule' },
     { path: 'categories', loadChildren: './pages/categories/categories.module#CategoriesModule' }
